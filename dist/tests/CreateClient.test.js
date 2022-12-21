249ec5822ac829da37971b048714f65e42adeac7
Leandro Abreu <l.leandro_abreu@hotmail.com>
2022-12-21 16:06:15 -0300

    add authentication middleware

--- dist/tests/CreateClient.test.js
+++ dist/tests/CreateClient.test.js
@@ -5,8 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = __importDefault(require("axios"));
 test('should verify if client exists', () => {
-    axios_1.default.post('http://localhost:3000/deliveryman/authenticate', {
-        username: 'nowras',
-        password: 'senhasecreta'
+    axios_1.default.post('http://localhost:3000/delivery', {
+        id_client: 'afed5e52-801a-429c-9416-7bd53826aa08',
+        item_name: 'mouse usb'
     });
 });
--- src/routes.ts
+++ src/routes.ts
@@ -1,18 +1,22 @@
+import { CreateDeliveryController } from './modules/deliveries/useCases/createDelivery/CreateDeliveryController';
 import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController';
 import { AuthenticateClientController } from './modules/account/authenticateClient/AuthenticateClientController';
 import { Router } from "express";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
 import { AuthenticateDeliverymanController } from './modules/account/authenticateDeliveryman/AuthenticateDeliverymanController';
+import { ensureAuthenticateClient } from './middlewares/ensureAuthenticateClient';
 
 const routes = Router()
 const createClientController = new CreateClientController()
 const authenticateClientController = new AuthenticateClientController()
 const createDeliverymanController = new CreateDeliverymanController() 
 const authenticateDelirymanController = new AuthenticateDeliverymanController()
+const createDeliveryController = new CreateDeliveryController()
 
 routes.post('/client/', createClientController.handle)
 routes.post('/client/authenticate/', authenticateClientController.handle)
 routes.post('/deliveryman/authenticate/', authenticateDelirymanController.handle)
 routes.post('/deliveryman/', createDeliverymanController.handle)
+routes.post('/delivery/', ensureAuthenticateClient, createDeliveryController.handle)
 
 export { routes }
