60b244f99a334177abc2ab5975dd21be2fec3265
Leandro Abreu <l.leandro_abreu@hotmail.com>
2023-01-24 09:40:41 -0300

    refactor routes file

--- scripts.sh
+++
@@ -1,2 +0,0 @@
-npx primas migrate dev
-    create_deliveryman_clients_deliveries
--- tests/CreateClient.test.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-
-test('should verify if client exists', () =>{
-  axios.post('http://localhost:3000/client/authenticate', {
-    id_client: 'afed5e52-801a-429c-9416-7bd53826aa08',
-    item_name: 'mouse usb'
-  })
-})
